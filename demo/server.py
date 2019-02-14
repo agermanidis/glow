7795fa772fe306890bc94a5b0e47182c0f33446e
Gene Kogan <kogan.gene@gmail.com>
2019-02-13 20:04:00 -0500

    runway model

--- demo/server.py
+++ demo/server.py
@@ -1,228 +1,148 @@
-import model
-from align_face import align_face
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-
-import base64
-import time
+from runway import RunwayModel
+from model import *
+from imutils.face_utils.helpers import FACIAL_LANDMARKS_68_IDXS
+from imutils.face_utils.helpers import FACIAL_LANDMARKS_5_IDXS
+from imutils.face_utils.helpers import shape_to_np
 import numpy as np
-from PIL import Image
-from io import BytesIO
-app = Flask(__name__)
-CORS(app)
+import cv2
+import dlib
 
 
-def deserialise_img(img_str):
-    img = base64.b64decode(img_str.split(",")[-1])
-    img = Image.open(BytesIO(img))
-    img = img.convert('RGB')
-    img = np.array(img)
-    return img
+class FaceWarper:
+    def __init__(self, predictor, desiredLeftEye=(0.35, 0.35),
+        desiredFaceWidth=512, desiredFaceHeight=None):
+        # store the facial landmark predictor, desired output left
+        # eye position, and desired output face width + height
+        self.predictor = predictor
+        self.desiredLeftEye = desiredLeftEye
+        self.desiredFaceWidth = desiredFaceWidth
+        self.desiredFaceHeight = desiredFaceHeight
+
+        # if the desired face height is None, set it to be the
+        # desired face width (normal behavior)
+        if self.desiredFaceHeight is None:
+            self.desiredFaceHeight = self.desiredFaceWidth
+
+    def align(self, image, gray, rect, z_addition):
+        # convert the landmark (x, y)-coordinates to a NumPy
+        h1, w1 = image.shape[:2]
+        shape = self.predictor(gray, rect)
+        shape = shape_to_np(shape)
+
+        #simple hack ;)
+        if (len(shape)==68):
+            # extract the left and right eye (x, y)-coordinates
+            (lStart, lEnd) = FACIAL_LANDMARKS_68_IDXS["left_eye"]
+            (rStart, rEnd) = FACIAL_LANDMARKS_68_IDXS["right_eye"]
+        else:
+            (lStart, lEnd) = FACIAL_LANDMARKS_5_IDXS["left_eye"]
+            (rStart, rEnd) = FACIAL_LANDMARKS_5_IDXS["right_eye"]
+
+        leftEyePts = shape[lStart:lEnd]
+        rightEyePts = shape[rStart:rEnd]
+
+        # compute the center of mass for each eye
+        leftEyeCenter = leftEyePts.mean(axis=0).astype("int")
+        rightEyeCenter = rightEyePts.mean(axis=0).astype("int")
+
+        # compute the angle between the eye centroids
+        dY = rightEyeCenter[1] - leftEyeCenter[1]
+        dX = rightEyeCenter[0] - leftEyeCenter[0]
+        angle = np.degrees(np.arctan2(dY, dX)) - 180
+
+        # compute the desired right eye x-coordinate based on the
+        # desired x-coordinate of the left eye
+        desiredRightEyeX = 1.0 - self.desiredLeftEye[0]
+
+        # determine the scale of the new resulting image by taking
+        # the ratio of the distance between eyes in the *current*
+        # image to the ratio of distance between eyes in the
+        # *desired* image
+        dist = np.sqrt((dX ** 2) + (dY ** 2))
+        desiredDist = (desiredRightEyeX - self.desiredLeftEye[0])
+        desiredDist *= self.desiredFaceWidth
+        scale = desiredDist / dist
+
+        # compute center (x, y)-coordinates (i.e., the median point)
+        # between the two eyes in the input image
+        eyesCenter = ((leftEyeCenter[0] + rightEyeCenter[0]) // 2,
+            (leftEyeCenter[1] + rightEyeCenter[1]) // 2)
+
+        # grab the rotation matrix for rotating and scaling the face
+        M = cv2.getRotationMatrix2D(eyesCenter, angle, scale)
+
+        # update the translation component of the matrix
+        tX = self.desiredFaceWidth * 0.5
+        tY = self.desiredFaceHeight * self.desiredLeftEye[1]
+        M[0, 2] += (tX - eyesCenter[0])
+        M[1, 2] += (tY - eyesCenter[1])
+
+        # apply the affine transformation
+        (w, h) = (self.desiredFaceWidth, self.desiredFaceHeight)
+        output = cv2.warpAffine(image, M, (w, h),
+            flags=cv2.INTER_CUBIC)
+
+        # invert the previous affine transformation for later
+        Mi = cv2.invertAffineTransform(M)
+
+        # BGR -> RGB
+        output = output[:,:,::-1]
+
+        # encode with GLOW, do operations on z
+        z = encode(output)
+        z[0] += z_addition
+
+        # decode back to image and back to BGR
+        output = decode(z)[0]
+        output = output[:,:,::-1]
+
+        # invert the affine transformation on output
+        output = cv2.warpAffine(output, Mi, (w1, h1),
+            flags=cv2.INTER_CUBIC)
+
+        # overwrite original image with masked output
+        mask = np.sum(output, axis=2) == 0.0
+        image = np.multiply(mask.reshape((h1, w1, 1)), image)
+        image += output
+
+        return image
 
 
-def serialise_img(arr):
-    img = Image.fromarray(arr)
-    buf = BytesIO()
-    img.save(buf, format='PNG')
-    buf = buf.getvalue()
-    return "data:image/png;base64," + base64.b64encode(buf).decode('utf-8')
+# load face detection and warping
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+warper = FaceWarper(predictor, desiredFaceWidth=256, desiredLeftEye=(0.371, 0.480))
+
+# tags that can be modified
+tags = "5_o_Clock_Shadow Arched_Eyebrows Attractive Bags_Under_Eyes Bald Bangs Big_Lips Big_Nose Black_Hair Blond_Hair Blurry Brown_Hair Bushy_Eyebrows Chubby Double_Chin Eyeglasses Goatee Gray_Hair Heavy_Makeup High_Cheekbones Male Mouth_Slightly_Open Mustache Narrow_Eyes No_Beard Oval_Face Pale_Skin Pointy_Nose Receding_Hairline Rosy_Cheeks Sideburns Smiling Straight_Hair Wavy_Hair Wearing_Earrings Wearing_Hat Wearing_Lipstick Wearing_Necklace Wearing_Necktie Young"
+tags = tags.split()
+
+# launch Runway
+glow = RunwayModel()
+model_ready = False
+
+@glow.setup
+def setup():
+	global model_ready
+	print('setup model')
+	model_ready = True
+	return None
 
 
-def deserialise_nparr(arr_str):
-    arr = np.loads(base64.b64decode(arr_str))
-    return np.array(arr, dtype=np.float32)
+@glow.command('detect', inputs={'image': 'image'}, outputs={'output': 'image'})
+def detect(sess, inp):
+	img = np.array(inp['image'])
+	z_addition = 1.1 * z_manipulate[tags.index('Blond_Hair')]
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	rects = detector(gray, 2)
+	if len(rects) == 0 or not model_ready:
+		print('nothing found')
+		return dict(output=img)
+	img = warper.align(img[:, :, ::-1], gray, rects[0], z_addition)[:, :, ::-1]
+	img = np.array(Image.fromarray(img).convert('RGB'))
+	output = np.clip(img, 0, 255).astype(np.uint8)
+	return dict(output=output)
 
 
-def serialise_nparr(arr):
-    arr = np.array(arr, dtype=np.float16)
-    return base64.b64encode(arr.dumps()).decode('utf-8')
-
-
-def send(result):
-    # img, z are batches, send as list of singles
-    img, z = result
-    # , z=list(map(serialise_nparr, z)))
-    return jsonify(img=list(map(serialise_img, img)))
-
-
-def send_proj(result, proj):
-    # img, z are batches, send as list of singles
-    img, z = result
-    return jsonify(face_found=True, img=list(map(serialise_img, img)), z=list(map(serialise_nparr, z)), proj=proj.tolist())
-
-
-def get(request, key):
-    return request.get_json().get(key)
-
-
-def get_z(request, key):
-    # z is a single point, batch it for use
-    z = get(request, key)
-    return np.expand_dims(deserialise_nparr(z), axis=0)
-
-
-@app.route('/')
-def hello_world():
-    return 'Welcome to Glow!'
-
-# Align and encode image
-#
-# args
-# img: Image as base64 string
-#
-# returns
-# json: {'face_found': face_found, 'img':[base64 img], 'z': [serialised z]}
-@app.route('/api/align_encode', methods=['POST'])
-def align_encode():
-    r = request
-    img = get(r, 'img')
-    # img = parse_img(img) if in jpg etc format
-    img = deserialise_img(img)
-    img, face_found = align_face(img)
-    if face_found:
-        img = np.reshape(img, [1, 256, 256, 3])
-        print(img.shape)
-        z = model.encode(img)
-        proj = model.project(z)  # get projections. Not used
-        result = img, z
-        # jsonify(img=serialise_img(img), z=serialise_nparr(z))
-        return send_proj(result, proj)
-    else:
-        return jsonify(face_found=False)
-
-# Maipulate single attribute
-#
-# args
-# z: Serialised np array for encoding of image
-# typ: int in [0,40), representing which attribute to manipulate
-# alpha: float, usually in [-1,1], representing how much to manipulate. 0 gives original image
-#
-# returns
-# json: {'img': [img]}
-@app.route('/api/manipulate', methods=['POST'])
-def manipulate():
-    r = request
-    z = get_z(r, 'z')
-    typ = get(r, 'typ')
-    alpha = get(r, 'alpha')
-    result = model.manipulate(z, typ, alpha)
-    return send(result)
-
-# Manipulate multiple attributes
-# typs: list of typ
-# alphas: list of corresponding alphas
-@app.route('/api/manipulate_all', methods=['POST'])
-def manipulate_all():
-    r = request
-    z = get_z(r, 'z')
-    typs = get(r, 'typs')
-    alphas = get(r, 'alphas')
-    result = model.manipulate_all(z, typs, alphas)
-    return send(result)
-
-# Mix two faces
-#
-# args
-# z1: Serialised np array for encoding of image 1
-# z2: Serialised np array for encoding of image 2
-# alpha: float in [0,1], representing how much to mix. 0.5 gives middle image
-#
-# returns
-# json: {'img': [img]}
-@app.route('/api/mix', methods=['POST'])
-def mix():
-    r = request
-    z1 = get_z(r, 'z1')
-    z2 = get_z(r, 'z2')
-    alpha = get(r, 'alpha')
-    result = model.mix(z1, z2, alpha)
-    return send(result)
-
-# Get random image
-@app.route('/api/random', methods=['POST'])
-def random():
-    r = request
-    bs = get(r, 'bs')
-    result = model.random(bs)
-    img, z = result
-    proj = model.project(z)
-    return send_proj(result, proj)
-
-# Extra functions
-@app.route('/api/test', methods=['POST'])
-def test():
-    r = request
-    z = get_z(r, 'z')
-    typs = get(r, 'typs')
-    alphas = get(r, 'alphas')  # value between [-1,1] -> 0.5 is original image
-    return jsonify(z="")
-
-
-@app.route('/api/manipulate_range', methods=['POST'])
-def manipulate_range():
-    r = request
-    z = get_z(r, 'z')
-    typ = get(r, 'typ')
-    points = get(r, 'points')
-    result = model.manipulate_range(z, typ, points)
-    return send(result)
-
-
-@app.route('/api/mix_range', methods=['POST'])
-def mix_range():
-    r = request
-    z1 = get_z(r, 'z1')
-    z2 = get_z(r, 'z2')
-    points = get(r, 'points')
-    result = model.mix_range(z1, z2, points)
-    return send(result)
-
-# Legacy
-# @app.route('/api/encode', methods=['POST'])
-# def encode():
-#     t = time.time()
-#     r = request
-#     img = get(r, 'img')
-#     print("Time to read from request", time.time() - t)
-#     t = time.time()
-#     img = deserialise_nparr(img)
-#     print("Time to serialise from request", time.time() - t)
-#     t = time.time()
-#     z = model.encode(img)
-#     print("TIme to encode", time.time() - t)
-#     t = time.time()
-#     json = jsonify(z=serialise_nparr(z))
-#     print("Time to jsonify", time.time() - t)
-#     return json
-#
-# @app.route('/api/decode', methods=['POST'])
-# def decode():
-#     t = time.time()
-#     r = request
-#     z = get(r, 'z')
-#     print("Time to read from request", time.time() - t)
-#     t = time.time()
-#     z = deserialise_nparr(z)
-#     print("Time to serialise from request", time.time() - t)
-#     t = time.time()
-#     img = model.decode(z)
-#     print("TIme to decode", time.time() - t)
-#     t = time.time()
-#     json = jsonify(img=serialise_img(img))
-#     print("Time to jsonify", time.time() - t)
-#     return json
-#
-# @app.route('/api/align', methods=['POST'])
-# def align():
-#     r = request
-#     img = get(r, 'img')
-#     # img = parse_img(img) if in jpg etc format
-#     img = deserialise_img(img)
-#     img = align_face(img)
-#     return jsonify(img=serialise_img(img))
-
-
-# FaceOff! Use for manipulation and blending faces
 if __name__ == '__main__':
-    print('Running Flask app...')
-    app.run(host='0.0.0.0', port=5050)
+    glow.run()
